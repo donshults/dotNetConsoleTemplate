83ef5ef254d65a0326fdb4897c8a4833a4b9d83f
don.shults <donald.shults@precastcorp.com>
2020-12-04 10:28:53 -0800

    Current Working

--- dotNetConsoleApp/dotNetConsole/Services/APIClient.cs
+++ dotNetConsoleApp/dotNetConsole/Services/APIClient.cs
@@ -1,17 +1,13 @@
 ﻿using dotNetConsole.Auth;
-using dotNetConsole.Models;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
